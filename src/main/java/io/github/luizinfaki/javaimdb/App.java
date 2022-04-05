aa5b1f38fd84006f366c0b16cb88ccab19eac4f3
luizinfaki <gfxinvite@gmail.com>
2022-04-04 23:41:49 -0300

    json string handle w org.json

--- src/main/java/io/github/luizinfaki/javaimdb/App.java
+++ src/main/java/io/github/luizinfaki/javaimdb/App.java
@@ -1,18 +1,26 @@
 package io.github.luizinfaki.javaimdb;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         Dotenv dotenv = Dotenv.load();
         final String MY_API_KEY = dotenv.get("MY_IMDB_API_KEY", "");
+
+        List<String> urlImages = new ArrayList<>();
+        List<String> titles = new ArrayList<>();
+        List<String> years = new ArrayList<>();
+        List<String> imDbRatings = new ArrayList<>();
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -23,6 +31,18 @@ public class App {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String json = response.body();
 
-        System.out.println(json);
+        JSONObject obj = new JSONObject(json);
+        JSONArray arrItems = obj.getJSONArray("items");
+
+        for (int i = 0; i < arrItems.length(); i++) {
+            urlImages.add(arrItems.getJSONObject(i).getString("image"));
+            titles.add(arrItems.getJSONObject(i).getString("title"));
+            years.add(arrItems.getJSONObject(i).getString("year"));
+            imDbRatings.add(arrItems.getJSONObject(i).getString("imDbRating"));
+        }
+
+        for (int i = 0; i < titles.size(); i++) {
+            System.out.printf("%d. %s (%s) - %s%n", i + 1, titles.get(i), years.get(i), imDbRatings.get(i));
+        }
     }
 }
